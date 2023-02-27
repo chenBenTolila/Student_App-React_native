832356ce54c6245dcd6ee65a843e7b7d80bef4d5
chenBenTolila <chenbt266@google.com>
2023-02-27 19:06:57 +0200

    adding profile screen and refresh token

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,134 +1,151 @@
-// import { FC, useState, useEffect } from "react";
-// import {
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   View,
-//   Image,
-//   TouchableOpacity,
-//   Button,
-//   Alert,
-//   TextInput,
-//   FlatList,
-//   TouchableHighlight,
-// } from "react-native";
+import { FC, useState, useEffect } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+  TextInput,
+  FlatList,
+  TouchableHighlight,
+} from "react-native";
 
-// import PostModel, { Post } from "../model/PostModel";
+import PostModel, { Post } from "../model/PostModel";
 
-// const ListItem: FC<{
-//   name: String;
-//   id: String;
-//   image: String;
-//   onRowSelected: (id: String) => void;
-// }> = ({ name, id, image, onRowSelected }) => {
-//   const onClick = () => {
-//     console.log("int he row: row was selected " + id);
-//     onRowSelected(id);
-//   };
+const ListItem: FC<{
+  // postId: String;
+  userImage: String;
+  userId: String;
+  text: String;
+  postImage: String;
+  //onPostSelected: (postId: String) => void;
+}> = ({ userId, userImage, text, postImage }) => {
+  // const onClick = () => {
+  //   console.log("int post: post was selected " + postId);
+  //   onPostSelected(postId);
+  // };
 
-//   console.log("image: " + image);
-//   return (
-//     <TouchableHighlight onPress={onClick} underlayColor={"gainsboro"}>
-//       <View style={styles.listRow}>
-//         {image == "" && (
-//           <Image
-//             style={styles.listRowImage}
-//             source={require("../assets/avatar.png")}
-//           />
-//         )}
-//         {image != "" && (
-//           <Image
-//             style={styles.listRowImage}
-//             source={{ uri: image.toString() }}
-//           />
-//         )}
+  console.log("image: " + postImage);
+  return (
+    <TouchableHighlight underlayColor={"gainsboro"}>
+      <View style={styles.listRow}>
+        {userImage == "" && (
+          <Image
+            style={styles.listRowImage}
+            source={require("../assets/avatar.png")}
+          />
+        )}
+        {userImage != "" && (
+          <Image
+            style={styles.listRowImage}
+            source={{ uri: userImage.toString() }}
+          />
+        )}
+        <Text style={styles.listRowName}>{userId}</Text>
 
-//         <View style={styles.listRowTextContainer}>
-//           <Text style={styles.listRowName}>{name}</Text>
-//           <Text style={styles.listRowId}>{id}</Text>
-//         </View>
-//       </View>
-//     </TouchableHighlight>
-//   );
-// };
+        {postImage == "" && (
+          <Image
+            style={styles.listRowImage}
+            source={require("../assets/avatar.png")}
+          />
+        )}
+        {postImage != "" && (
+          <Image
+            style={styles.listRowImage}
+            source={{ uri: postImage.toString() }}
+          />
+        )}
+        <Text style={styles.listRowName}>{userId}</Text>
 
-// const StudentList: FC<{ route: any; navigation: any }> = ({
-//   route,
-//   navigation,
-// }) => {
-//   const onRowSelected = (id: String) => {
-//     console.log("in the list: row was selected " + id);
-//     navigation.navigate("StudentDetails", { studentId: id });
-//   };
+        {/* <View style={styles.listRowTextContainer}>
+          <Text style={styles.listRowName}>{name}</Text>
+          <Text style={styles.listRowId}>{id}</Text>
+        </View> */}
+      </View>
+    </TouchableHighlight>
+  );
+};
 
-//   const [students, setStudents] = useState<Array<Student>>();
+const PostsList: FC<{ route: any; navigation: any }> = ({
+  route,
+  navigation,
+}) => {
+  const onPostSelected = (id: String) => {
+    console.log("in the list: row was selected " + id);
+    //navigation.navigate("StudentDetails", { studentId: id });
+  };
 
-//   useEffect(() => {
-//     const unsubscribe = navigation.addListener("focus", async () => {
-//       console.log("focus");
-//       let students: Student[] = [];
-//       try {
-//         students = await StudentModel.getAllStudents();
-//         console.log("fetching students complete");
-//       } catch (err) {
-//         console.log("fail fetching students " + err);
-//       }
-//       console.log("fetching finish");
-//       setStudents(students);
-//     });
-//     return unsubscribe;
-//   });
+  const [posts, setPosts] = useState<Array<Post>>();
 
-//   return (
-//     <FlatList
-//       style={styles.flatlist}
-//       data={students}
-//       keyExtractor={(student) => student.id.toString()}
-//       renderItem={({ item }) => (
-//         <ListItem
-//           name={item.name}
-//           id={item.id}
-//           image={item.image}
-//           onRowSelected={onRowSelected}
-//         />
-//       )}
-//     ></FlatList>
-//   );
-// };
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", async () => {
+      console.log("focus");
+      let posts: Post[] = [];
+      try {
+        posts = await PostModel.getAllPosts();
+        console.log("fetching students complete");
+      } catch (err) {
+        console.log("fail fetching students " + err);
+      }
+      console.log("fetching finish");
+      setPosts(posts);
+    });
+    return unsubscribe;
+  });
 
-// const styles = StyleSheet.create({
-//   container: {
-//     marginTop: StatusBar.currentHeight,
-//     flex: 1,
-//     backgroundColor: "grey",
-//   },
-//   flatlist: {
-//     flex: 1,
-//   },
-//   listRow: {
-//     margin: 4,
-//     flexDirection: "row",
-//     height: 150,
-//     elevation: 1,
-//     borderRadius: 2,
-//   },
-//   listRowImage: {
-//     margin: 10,
-//     resizeMode: "contain",
-//     height: 130,
-//     width: 130,
-//   },
-//   listRowTextContainer: {
-//     flex: 1,
-//     margin: 10,
-//     justifyContent: "space-around",
-//   },
-//   listRowName: {
-//     fontSize: 30,
-//   },
-//   listRowId: {
-//     fontSize: 25,
-//   },
-// });
+  return (
+    <FlatList
+      style={styles.flatlist}
+      data={posts}
+      keyExtractor={(post) => post.postId.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          userId={item.userId}
+          text={item.text}
+          postImage={item.image}
+          userImage={item.userImage}
+        />
+      )}
+    ></FlatList>
+  );
+};
 
-// export default StudentList;
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  flatlist: {
+    flex: 1,
+  },
+  listRow: {
+    margin: 4,
+    flexDirection: "row",
+    height: 150,
+    elevation: 1,
+    borderRadius: 2,
+  },
+  listRowImage: {
+    margin: 10,
+    resizeMode: "contain",
+    height: 130,
+    width: 130,
+  },
+  listRowTextContainer: {
+    flex: 1,
+    margin: 10,
+    justifyContent: "space-around",
+  },
+  listRowName: {
+    fontSize: 30,
+  },
+  listRowId: {
+    fontSize: 25,
+  },
+});
+
+export default PostsList;
